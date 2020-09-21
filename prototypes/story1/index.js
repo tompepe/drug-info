5aa8d2f6017e4419808a1580f59c96309eeb2a3c
tom.pepe <tom.pepe@wwt.com>
2020-09-21 12:29:05 -0500

    add typescript and add prototype view for design trial/error etc

---
+++ prototypes/story1/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { DrugView } from "./src/Components/DrugView/DrugView";
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<DrugView />, mountNode);
