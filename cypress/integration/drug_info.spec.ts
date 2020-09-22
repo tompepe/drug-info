837e5b3b8a55b52f95cd3806201507d2f6dd8ce1
tom.pepe <tom.pepe@wwt.com>
2020-09-22 15:14:39 -0500

    introduce jest and fix jest cypress conflicts

--- cypress/integration/drug_info.spec.ts
+++ cypress/integration/drug_info.spec.ts
@@ -1,16 +1,15 @@
-const screen = cy;
-
 describe('Drug info', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('display the generic name of a drug', () => {
-    screen.findByText(/generic name: rizatriptan/i).as('findRizatriptan');
+    cy.findByText(/generic name: rizatriptan/i).as('findRizatriptan');
     cy.get('@findRizatriptan', {
       timeout: 50000,
       requestTimeout: 50000,
       responseTimeout: 100000,
-    }).should('have.length', 1);
+    } as Partial<Cypress.Timeoutable>).should('have.length', 1);
   });
 });
+
---
+++ src/Components/DrugInteraction/DrugInteraction.spec.ts
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import jest from 'jest';
+
+describe('<DrugInteraction />', () => {
+  // beforeEach(async () => {
+  //     jest.clearAllMocks();
+
+  //     renderTestObject();
+
+  //     expect(await screen.findByTestId('page-header')).toHaveTextContent('Create Approval Routing');
+  // });
+
+  it('should render a page header and labels', () => {
+    expect(true).toEqual(false);
+  });
+});
